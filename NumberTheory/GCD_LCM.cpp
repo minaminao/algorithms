55e4c772724e71fcc44c6446ff4217ba3161ae01
minami <minaminaoy@gmail.com>
2019-03-19 18:05:25 +0900

    多倍長整数用の GCD を追加

--- NumberTheory/GCD_LCM.cpp
+++ NumberTheory/GCD_LCM.cpp
@@ -5,6 +5,7 @@
 
 int gcd(int x, int y) { return y ? gcd(y, x % y) : x; }
 int lcm(int x, int y) { return x / gcd(x, y) * y; }
+
 int gcd(const vector<int> &v) {
 	int ret = v[0];
 	for (int i = 1; i < v.size(); i++)
@@ -16,4 +17,17 @@ int lcm(const vector<int> &v) {
 	for (int i = 1; i < v.size(); i++)
 		ret = lcm(ret, v[i]);
 	return ret;
+}
+
+// 非再帰
+// 多倍長整数用
+using Int = int;
+Int gcd(Int x, Int y) {
+	while (true) {
+		if (y == 0)
+			return x;
+		Int t = x;
+		x = y;
+		y = t % y;
+	}
 }
