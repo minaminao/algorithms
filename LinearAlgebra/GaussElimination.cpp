b8dd5ad30b5f12cb2728ed48ab10206822faf365
minami <minaminaoy@gmail.com>
2019-03-21 21:48:33 +0900

    微修正

--- LinearAlgebra/GaussElimination.cpp
+++ LinearAlgebra/GaussElimination.cpp
@@ -1,28 +1,29 @@
 // ガウスの消去法（Gauss elimination）
 // O(n^3)
 //
-// 戻り値: (解が存在するか, rank, 解）
+// 戻り値: (解が存在するか, rank, 解)
 //
 // Verified:
 //   http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3437138
 //   http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3437187
 //   https://yukicoder.me/submissions/326809 (mod)
 
-using Num = mint;
+using Num = double;
 using Vec = vector<Num>;
 using Mat = vector<Vec>;
 tuple<bool, int, Vec> gaussianElimination(Mat A, Vec b) {
+	const Num EPS = 1e-5; // 誤差
 	const int n = A.size(), m = A[0].size();
 	int rank = 0, cj = 0;
 	while (rank < n && cj < m) {
 		// A[rank][cj] が最大になるように
 		for (int i = rank + 1; i < n; i++) {
-			if (A[i][cj].get() > A[rank][cj].get()) {
+			if (abs(A[i][cj]) > abs(A[rank][cj])) {
 				A[i].swap(A[rank]);
 				swap(b[i], b[rank]);
 			}
 		}
-		if (A[rank][cj].get() > 0) {
+		if (abs(A[rank][cj]) > 0) {
 			// 係数を 1 に
 			Num d = A[rank][cj];
 			for (int j = 0; j < m; j++)
@@ -41,7 +42,7 @@ tuple<bool, int, Vec> gaussianElimination(Mat A, Vec b) {
 	}
 	// 0 != b[i] だったら不能
 	for (int i = rank; i < n; i++)
-		if (b[i].get())
+		if (abs(b[i]) > EPS)
 			return make_tuple(false, rank, Vec());
 	// 不定
 	// rank != m
