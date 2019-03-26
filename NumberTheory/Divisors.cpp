caf2eee79799ccb8f8c3906dc691eab2b37305db
minami <minaminaoy@gmail.com>
2019-03-27 04:22:04 +0900

    Add an algorithm to calculate number of divisors

--- NumberTheory/Divisors.cpp
+++ NumberTheory/Divisors.cpp
@@ -1,4 +1,6 @@
-// 約数 未ソート
+// 約数
+// 未ソート
+// O(√n), O(2^(b/2))
 vector<int> getDivisors(int x) {
 	vector<int> ret;
 	int i;
@@ -10,4 +12,18 @@ vector<int> getDivisors(int x) {
 	if (i*i == x)
 		ret.emplace_back(i);
 	return ret;
+}
+
+// 約数の個数の配列
+// 1 から n まで
+// O(n log n)
+//   n + n / 2 + n / 3 + ... + n / n = n log n
+// Verified:
+//   https://yukicoder.me/submissions/329249
+vector<int> numberOfDivisors(int n) {
+	vector<int> num(n + 1);
+	for (int i = 1; i <= n; i++)
+		for (int j = i; j <= n; j += i)
+			num[j]++;
+	return num;
 }
