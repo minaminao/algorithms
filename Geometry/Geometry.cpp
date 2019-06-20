28cbdc81d2d88af56e774db29c25f96493388c30
minami <minaminaoy@gmail.com>
2019-06-20 21:54:34 +0900

    Pointをcomplexでの実装に

--- Geometry/Geometry.cpp
+++ Geometry/Geometry.cpp
@@ -6,45 +6,31 @@ const double PI = acos(-1);
 #define prev(P,i) P[(i+P.size()-1)%P.size()]
 
 // 点
-struct Point {
-	double x, y;
-	Point() :x(0.0), y(0.0) {}
-	Point(double x, double y) :x(x), y(y) {}
-
-	double abs() { return sqrt(norm()); }
-	double norm() { return x * x + y * y; }
-};
-Point operator+(const Point &p1, const Point &p2) { return Point(p1.x + p2.x, p1.y + p2.y); }
-Point operator-(const Point &p1, const Point &p2) { return Point(p1.x - p2.x, p1.y - p2.y); }
-Point operator*(const Point &p, double a) { return Point(p.x * a, p.y * a); }
-Point operator/(const Point &p, double a) { return Point(p.x / a, p.y / a); }
-bool operator==(const Point &p1, const Point &p2) { return equals(p1.x, p2.x) && equals(p1.y, p2.y); }
+#define X real()
+#define Y imag()
+using Point = complex<double>;
+bool operator==(const Point &p1, const Point &p2) { return equals(p1.X, p2.X) && equals(p1.Y, p2.Y); }
 bool cmpx(const Point &p1, const Point &p2) {
-	return p1.x != p2.x ? p1.x < p2.x : p1.y < p2.y; // x昇順 -> y昇順
+	return p1.X != p2.X ? p1.X < p2.X : p1.Y < p2.Y; // x昇順 -> y昇順
 }
 bool cmpy(const Point &p1, const Point &p2) {
-	return p1.y != p2.y ? p1.y < p2.y : p1.x < p2.x; // y昇順 -> x昇順
+	return p1.Y != p2.Y ? p1.Y < p2.Y : p1.X < p2.X; // y昇順 -> x昇順
 }
 bool operator<(const Point &p1, const Point &p2) {
 	return cmpx(p1, p2); // 問題に応じて cmpy
 }
 bool operator>(const Point &p1, const Point &p2) { return p2 < p1; }
 inline istream &operator >> (istream &is, Point &p) { double x, y; is >> x >> y; p = Point(x, y); return is; }
-inline ostream &operator << (ostream &os, const Point &p) { os << p.x << ' ' << p.y; return os; }
+inline ostream &operator << (ostream &os, const Point &p) { os << p.X << ' ' << p.Y; return os; }
 
 // ベクトル
-struct Vector :public Point {
-	using Point::Point;
-	Vector() {}
-	Vector(const Point &P) { x = P.x; y = P.y; }
-	Vector rotate(double rad) { return Vector(x*cos(rad) - y * sin(rad), x*sin(rad) + y * cos(rad)); }
-	Vector unit() { return *this / abs(); }
-};
-
+using Vector = Point;
+Vector rotate(Vector vec, double rad) { return Vector(vec.X*cos(rad) - vec.Y * sin(rad), vec.X*sin(rad) + vec.Y * cos(rad)); }
+Vector unit(Vector vec) { return vec / abs(vec); }
 // 内積（dot product）
-double dot(Vector a, Vector b) { return a.x*b.x + a.y*b.y; }
+double dot(Vector a, Vector b) { return a.X*b.X + a.Y*b.Y; }
 // 外積（cross product）の大きさ（正負あり）
-double cross(Vector a, Vector b) { return a.x*b.y - a.y*b.x; }
+double cross(Vector a, Vector b) { return a.X*b.Y - a.Y*b.X; }
 
 // 直線
 struct Line {
@@ -60,7 +46,6 @@ struct Segment :public Line {
 	Segment(const Line &L) { p1 = L.p1; p2 = L.p2; }
 	Vector vec() { return p2 - p1; }
 };
-inline ostream &operator << (ostream &os, const Segment &s) { os << s.p1 << ' ' << s.p2; return os; }
 
 // 円
 struct Circle {
@@ -78,15 +63,9 @@ double rad(double deg) { return PI * deg / 180; }
 // radian to degree
 double deg(double rad) { return rad / PI * 180; }
 
-// 偏角 argument
-double arg(Vector p) { return atan2(p.y, p.x); }
-
-// 極形式 polar form
-Vector polar(double r, double a) { return Point(cos(a)*r, sin(a)*r); }
-
 // 2つのベクトルの角度
 double angle(Vector a, Vector b) {
-	double lena = a.abs(), lenb = b.abs();
+	double lena = abs(a), lenb = abs(b);
 	if (lena == 0 || lenb == 0)return 0; //解なし
 	double costheta = dot(a, b) / (lena*lenb);
 	if (equals(costheta, 1))costheta = 1; //誤差対策
@@ -94,7 +73,7 @@ double angle(Vector a, Vector b) {
 }
 
 bool inrange(Point p, double x1, double y1, double x2, double y2) {
-	return x1 <= p.x&&p.x <= x2 && y1 <= p.y&&p.y <= y2;
+	return x1 <= p.X&&p.X <= x2 && y1 <= p.Y&&p.Y <= y2;
 }
 
 // 直交判定
@@ -108,7 +87,7 @@ bool is_parallel(Segment s1, Segment s2) { return equals(cross(s1.p2 - s1.p1, s2
 // 射影
 Point project(Segment s, Point p) {
 	Vector base = s.p2 - s.p1;
-	double r = dot(p - s.p1, base) / base.norm();
+	double r = dot(p - s.p1, base) / norm(base);
 	return s.p1 + base * r;
 }
 // 反射
@@ -121,14 +100,14 @@ int ccw(Point p0, Point p1, Point p2) {
 	if (cross(a, b) > EPS)return COUNTER_CLOCKWISE;
 	if (cross(a, b) < -EPS)return CLOCKWISE;
 	if (dot(a, b) < -EPS)return ONLINE_BACK; //p2 p0 p1
-	if (a.norm() < b.norm())return ONLINE_FRONT; //p0 p1 p2
+	if (norm(a) < norm(b))return ONLINE_FRONT; //p0 p1 p2
 	return ON_SEGMENT;
 }
 int ccw(Vector a, Vector b) {
 	if (cross(a, b) > EPS)return COUNTER_CLOCKWISE;
 	if (cross(a, b) < -EPS)return CLOCKWISE;
 	if (dot(a, b) < -EPS)return ONLINE_BACK; //p2 p0 p1
-	if (a.norm() < b.norm())return ONLINE_FRONT; //p0 p1 p2
+	if (norm(a) < norm(b))return ONLINE_FRONT; //p0 p1 p2
 	return ON_SEGMENT;
 }
 
@@ -141,15 +120,15 @@ bool intersect(Segment a, Segment b) {
 // (交差判定続く)
 
 // 2点間の距離
-double get_distance(Point a, Point b) { return (a - b).abs(); }
+double get_distance(Point a, Point b) { return abs(a - b); }
 
 // 直線と点の距離
-double get_distance(Line l, Point p) { return abs(cross(l.p2 - l.p1, p - l.p1) / (l.p2 - l.p1).abs()); }
+double get_distance(Line l, Point p) { return abs(cross(l.p2 - l.p1, p - l.p1) / abs(l.p2 - l.p1)); }
 
 // 線分と点の距離
 double get_distance(Segment s, Point p) {
-	if (dot(s.p2 - s.p1, p - s.p1) < 0.0)return (p - s.p1).abs();
-	if (dot(s.p1 - s.p2, p - s.p2) < 0.0)return (p - s.p2).abs();
+	if (dot(s.p2 - s.p1, p - s.p1) < 0.0)return abs(p - s.p1);
+	if (dot(s.p1 - s.p2, p - s.p2) < 0.0)return abs(p - s.p2);
 	return get_distance(Line(s), p);
 }
 
@@ -189,8 +168,8 @@ Point get_cross_point(Segment a, Segment b) {
 pair<Point, Point> get_cross_points(Circle c, Line l) {
 	assert(intersect(c, l));
 	Vector pr = project(l, c.c);
-	Vector e = (l.p2 - l.p1) / (l.p2 - l.p1).abs();
-	double base = sqrt(c.r*c.r - (pr - c.c).norm());
+	Vector e = (l.p2 - l.p1) / abs(l.p2 - l.p1);
+	double base = sqrt(c.r*c.r - norm(pr - c.c));
 	return make_pair(pr + e * base, pr - e * base);
 }
 
@@ -198,7 +177,7 @@ pair<Point, Point> get_cross_points(Circle c, Line l) {
 pair<Point, Point> get_cross_points(Circle c1, Circle c2) {
 	int m = intersect(c1, c2);
 	assert(m != 4 && m != 0);
-	double d = (c1.c - c2.c).abs();
+	double d = abs(c1.c - c2.c);
 	double a = acos((c1.r*c1.r - c2.r*c2.r + d * d) / (2 * c1.r*d));
 	double t = arg(c2.c - c1.c);
 	return make_pair(c1.c + polar(c1.r, t + a), c1.c + polar(c1.r, t - a));
@@ -212,8 +191,8 @@ int contains(const Polygon &pl, Point p) {
 	for (int i = 0; i < n; i++) {
 		Point a = pl[i] - p, b = pl[(i + 1) % n] - p;
 		if (abs(cross(a, b)) < EPS&&dot(a, b) < EPS)return ON;
-		if (a.y > b.y)swap(a, b);
-		if (a.y < EPS&&EPS<b.y&&cross(a, b)>EPS)x = !x;
+		if (a.Y > b.Y)swap(a, b);
+		if (a.Y < EPS&&EPS<b.Y&&cross(a, b)>EPS)x = !x;
 	}
 	return (x ? IN : OUT);
 }
@@ -245,7 +224,7 @@ bool is_convex(Polygon g) {
 // Graham scan https://en.wikipedia.org/wiki/Graham_scan
 // 辺上の点を含めない
 Polygon convex_hull(Polygon P) {
-	sort(P.begin(), P.end());
+	sort(P.begin(), P.end(), cmpx);
 	Polygon up;
 	for (Point &p : P) {
 		while (up.size() > 1 && ccw(up[up.size() - 2], up[up.size() - 1], p) != CLOCKWISE)up.pop_back();
@@ -265,7 +244,7 @@ Polygon convex_hull(Polygon P) {
 // Graham scan https://en.wikipedia.org/wiki/Graham_scan
 // 辺上の点を含む
 Polygon convex_hull_with_points_online(Polygon P) {
-	sort(P.begin(), P.end());
+	sort(P.begin(), P.end(), cmpx);
 	Polygon up;
 	for (Point &p : P) {
 		int _ccw;
@@ -289,7 +268,7 @@ Polygon convex_hull_with_points_online(Polygon P) {
 // calipers https://en.wikipedia.org/wiki/Rotating_calipers
 double diameter(Polygon P) {
 	P = convex_hull(P);
-	auto mima = minmax_element(P.begin(), P.end());
+	auto mima = minmax_element(P.begin(), P.end(), cmpx);
 	int I = mima.first - P.begin();
 	int J = mima.second - P.begin();
 	double maxd = get_distance(P[I], P[J]);
@@ -314,7 +293,7 @@ double diameter(Polygon P) {
 Polygon rotate(const Polygon &P, double rad) {
 	Polygon ret;
 	for (auto &p : P)
-		ret.emplace_back(p.x*cos(rad) - p.y*sin(rad), p.x*sin(rad) + p.y*cos(rad));
+		ret.emplace_back(p.X*cos(rad) - p.Y*sin(rad), p.X*sin(rad) + p.Y*cos(rad));
 	return ret;
 }
 
@@ -328,7 +307,7 @@ double area(double a, double b, double c) {
 Point center(const Polygon &P) {
 	Point ret;
 	for (auto &p : P)ret = ret + p;
-	ret = ret / P.size();
+	ret = ret / (double)P.size();
 	return ret;
 }
 
@@ -348,6 +327,6 @@ Point get_center(const Point &p1, const Point &p2, const Point &p3) {
 // 点pを通る円cの接線における接点
 pair<Point, Point> get_tangent(const Circle &c, const Point &p) {
 	double d = get_distance(c.c, p);
-	Circle c2((c.c + p) / 2, d / 2);
+	Circle c2((c.c + p) / 2.0, d / 2.0);
 	return get_cross_points(c, c2);
 }
